6b7c024ec73a2b58d80cd82e124f15f0c1136c43
Keilor Baltodano Martínez <86636398+keilorbm1234@users.noreply.github.com>
2026-08-29 14:59:26 -0600

    feat(core): estructura base mvc, modelos de entidad, dao y adaptadores xml

---
+++ src/cr/ac/una/resourcemanager/model/Usuario.java
@@ -0,0 +1,24 @@
+package cr.ac.una.resourcemanager.model;
+
+public class Usuario {
+    protected String id;
+    protected String clave;
+    protected String rol;
+
+    public Usuario() {}
+
+    public Usuario(String id, String clave, String rol) {
+        this.id = id;
+        this.clave = clave;
+        this.rol = rol;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getClave() { return clave; }
+    public void setClave(String clave) { this.clave = clave; }
+
+    public String getRol() { return rol; }
+    public void setRol(String rol) { this.rol = rol; }
+}
