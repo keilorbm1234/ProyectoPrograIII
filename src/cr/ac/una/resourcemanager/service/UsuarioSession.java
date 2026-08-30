f20ac06c2a16861a8a961ead18a727a5aaeeac63
Fernanda <fernanda.alfaro.ortega@est.una.ac.cr>
2026-08-30 17:06:34 -0600

    conexion de funcionariosXmlDAO a AuthService y validaciones en controllerLogin del rol

--- src/cr/ac/una/resourcemanager/service/UsuarioSession.java
+++ src/cr/ac/una/resourcemanager/service/UsuarioSession.java
@@ -1,5 +1,15 @@
 package cr.ac.una.resourcemanager.service;
 
-public class UsuarioSesion {
+import cr.ac.una.resourcemanager.model.Usuario;
+
+public class UsuarioSession {
+    private static Usuario usuario;
+
+    public static Usuario getUsuario() {return usuario; }
+    public static void setUsuario(Usuario usuario) {UsuarioSession.usuario = usuario;}
+    public static void logout() {UsuarioSession.usuario = null;}
+    public static boolean isLoggedIn() {return UsuarioSession.usuario != null;}
+
+
 
 }
