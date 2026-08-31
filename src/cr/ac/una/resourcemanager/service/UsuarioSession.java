e1494e920dd371210c9930072b9c6aff3c298c74
Fernanda <fernanda.alfaro.ortega@est.una.ac.cr>
2026-08-31 15:51:14 -0600

    UsuarioSession correción

--- src/cr/ac/una/resourcemanager/service/UsuarioSession.java
+++ src/cr/ac/una/resourcemanager/service/UsuarioSession.java
@@ -1,15 +1,22 @@
 package cr.ac.una.resourcemanager.service;
-
 import cr.ac.una.resourcemanager.model.Usuario;
 
 public class UsuarioSession {
     private static Usuario usuario;
 
-    public static Usuario getUsuario() {return usuario; }
-    public static void setUsuario(Usuario usuario) {UsuarioSession.usuario = usuario;}
-    public static void logout() {UsuarioSession.usuario = null;}
-    public static boolean isLoggedIn() {return UsuarioSession.usuario != null;}
+    public static Usuario getUsuario() {
+        return usuario;
+    }
 
+    public static void setUsuario(Usuario usuario) {
+        UsuarioSession.usuario = usuario;
+    }
 
+    public static void logout() {
+        UsuarioSession.usuario = null;
+    }
 
+    public static boolean isLoggedIn() {
+        return usuario != null;
+    }
 }
