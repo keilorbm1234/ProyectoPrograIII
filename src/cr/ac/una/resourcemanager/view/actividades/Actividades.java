3df7cb2c9ddac78c5ff6a21c0a9858b0c69c9842
SamuelCortesSanLee <samuel.cortes.sanlee@est.una.ac.cr>
2026-09-06 22:02:05 -0600

    Division en panelles separados

---
+++ src/cr/ac/una/resourcemanager/view/actividades/Actividades.java
@@ -0,0 +1,15 @@
+package cr.ac.una.resourcemanager.view.actividades;
+
+import com.github.lgooddatepicker.components.DatePicker;
+
+import javax.swing.*;
+
+public class Actividades {
+    private JButton cargarButton;
+    private JButton imprimirButton;
+    private JTable ActividadesSemanalesTable;
+    private JLabel semanaLabel;
+    private JLabel fechaRefLabel;
+    private DatePicker fechaRefDatePicker;
+    private JLabel actividSemLabel;
+}
